26efff50bc406fb68e4e5c4d45b8ce75812ba260
HuseyinCivan <huseyincivan@gmail.com>
2020-10-20 19:55:31 -0400

    injection updated

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -1,10 +1,27 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
+
+    //OfficeHours officeHours;
+
+    //always do your injections through interfaces !!! (config.xml de constructor-arg a ne eklemek istersey koy)
+    ExtraSessions extraSessions;
+
+
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : 20");
+        System.out.println("Weekly Teaching Hours : " + (20 + extraSessions.getHours()));
     }
 }
