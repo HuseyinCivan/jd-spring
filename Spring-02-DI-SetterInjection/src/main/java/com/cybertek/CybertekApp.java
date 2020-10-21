614b2ab13d48a71cca06158c1d09bcdafb757803
HuseyinCivan <huseyincivan@gmail.com>
2020-10-20 20:35:14 -0400

    setter injections

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,27 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        //old method
+        //Course course = new Java();
+
+        Course course = container.getBean("java",Course.class);
+
+        course.getTeachingHours();
+
+
+
+
+
+
+
+    }
+}
