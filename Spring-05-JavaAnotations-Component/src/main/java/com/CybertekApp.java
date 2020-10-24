f3031d0195b2f5bdeac5eac71782bf8869468e48
HuseyinCivan <huseyincivan@gmail.com>
2020-10-24 11:02:34 -0400

    Java Annotations - Component

--- Spring-05-JavaAnotations-Component/src/main/java/com/CybertekApp.java
+++ Spring-05-JavaAnotations-Component/src/main/java/com/CybertekApp.java
@@ -1,20 +1,18 @@
-package com.cybertek;
+package com;
 
 import com.cybertek.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 
 public class CybertekApp {
     public static void main(String[] args) {
 
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
-        Course course =container.getBean("java",Course.class);
+        Course course = container.getBean("API", Course.class);  //hepsi buyuk harfse buyuk harfle yaz!!
+        //  Course course =container.getBean("selenium",Course.class);  //ilk harfi kucuk harf yap!
+        //  Course course =container.getBean("java",Course.class);   //ilk harfi kucuk harf yap!
         course.getTeachingHours();
-
-
-        ((ClassPathXmlApplicationContext)container).close();
 
 
     }
