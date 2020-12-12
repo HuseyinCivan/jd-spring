e2b120e73454069d35d53d510b179f0c22f8b10c
HuseyinCivan <huseyincivan@gmail.com>
2020-12-12 14:17:01 -0500

    Spring-09-ORM-TableCreation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,6 +1,12 @@
 package com.cybertek.entity;
 
+import com.cybertek.enums.Gender;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -16,4 +22,32 @@ public class Student {
     private String last_name;
     @Column(name = "studentEmailAddress")
     private String email;
+
+    @Transient
+    private String city;
+
+
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime localTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+
+
 }
