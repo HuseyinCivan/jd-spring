6461daa96d739814ed444986f3ef4e2c31507b43
HuseyinCivan <huseyincivan@gmail.com>
2020-12-16 20:21:05 -0500

    Spring-09-ORM-Mapping-OneToMany

--- Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -1,11 +1,17 @@
 package com.cybertek.entity;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
 
     @Id
@@ -15,13 +21,15 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    @OneToMany(mappedBy = "person")
+    @OneToMany(mappedBy = "person",cascade = CascadeType.ALL)
     private List<Address> addresses;
 
-//    @OneToMany
+//    @OneToMany(cascade = CascadeType.ALL)
 //    @JoinColumn(name = "person_id")
 //    private List<Address> addresses;
 
-
-
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
