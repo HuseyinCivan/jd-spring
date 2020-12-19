397cfb39f3b25c2796d9eeb1bd577f704270e5a8
HuseyinCivan <huseyincivan@gmail.com>
2020-12-19 11:58:05 -0500

    Spring-09-ORM-CinemaLab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Setter
@@ -16,5 +18,11 @@ public class Cinema extends BaseEntity {
 
     private String sponsoredName;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
 
+    public Cinema(String name, String sponsoredName) {
+        this.name = name;
+        this.sponsoredName = sponsoredName;
+    }
 }
