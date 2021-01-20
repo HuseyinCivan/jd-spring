0443139687b7376cf58fe542da2d9bac7939be40
HuseyinCivan <huseyincivan@gmail.com>
2021-01-19 20:22:18 -0500

    Spring-10-Security-FormAuth

--- Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.cybertek.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,11 +8,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserPrincipalDetailsService userPrincipleDetailsSevice;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +32,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .defaultSuccessUrl("/index")
                 .failureUrl("/login?error=true")
-                .permitAll();
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("cybertekSecret")
+                .userDetailsService(userPrincipleDetailsSevice);
+
 
     }
 
