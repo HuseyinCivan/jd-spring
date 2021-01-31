01419b86a18ad13ab9e3a15af0e20167d7721fbe
HuseyinCivan <huseyincivan@gmail.com>
2021-01-30 19:06:15 -0500

    Spring-11-Rest-SchoolLab

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RestController
 public class HomeController {
@@ -31,6 +31,23 @@ public class HomeController {
         return restTemplate.getForObject(URL, Object.class,id);
 
     }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostsFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","lTE5abbDxdjGplutvTuc");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/api/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+        return response;
+
+    }
+
+
 
 
 
