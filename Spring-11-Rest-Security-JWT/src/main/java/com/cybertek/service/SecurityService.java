165ac46c0938349b8fcb1c37b57265eb0a885bcb
HuseyinCivan <huseyincivan@gmail.com>
2021-02-06 14:44:41 -0500

    Spring-11-Rest-Security-JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/service/SecurityService.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/service/SecurityService.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.nio.file.attribute.UserPrincipal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SecurityService implements UserDetailsService {
 
     private UserService userService;
@@ -23,20 +25,19 @@ public class SecurityService implements UserDetailsService {
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
         User foundUser = loadUser(s);
-        if (foundUser == null) {
-            throw new UsernameNotFoundException("User not found! " + s);
+        if(foundUser == null){
+            throw new UsernameNotFoundException("user not found! " + s);
         }
 
         return new org.springframework.security.core.userdetails.User(foundUser.getUsername(), foundUser.getPassword(), listAuthorities(foundUser));
-
     }
 
-    private User loadUser(String value) {
+    public User loadUser(String value){
         boolean isEmail = value.contains("@");
         return isEmail ? userService.readByEmail(value) : userService.readByUsername(value);
     }
 
-    private List<GrantedAuthority> listAuthorities(User user) {
+    private List<GrantedAuthority> listAuthorities(User user){
         List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
         grantedAuthorityList.add(new SimpleGrantedAuthority(user.getRole().name()));
         return grantedAuthorityList;
--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
@@ -1,14 +1,17 @@
 package com.cybertek.util;
 
 import com.cybertek.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JWTUtil {
@@ -16,23 +19,50 @@ public class JWTUtil {
     @Value("${security.jwt.secret-key}")
     private String secret = "cybertek";
 
-    public String generatePayload(User user, String username){
+    public String generatePayload(User user, String username) {
 
         Map<String, Object> claims = new HashMap<>();
-        claims.put("username",user.getUsername());
-        claims.put("email",user.getEmail());
-        return createToken(claims,username);
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+        return createToken(claims, username);
 
     }
 
-    private String createToken(Map<String, Object> claims, String username){
+    private String createToken(Map<String, Object> claims, String username) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10hours token validity
-                .signWith(SignatureAlgorithm.HS256,secret).compact();
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
 
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsRevolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsRevolver.apply(claims);
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username =extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && isTokenExpired(token));
     }
 
 }
