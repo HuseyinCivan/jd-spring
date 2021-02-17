891bbaff92b6a3a5ec96ead6bd1c35e2c02c0793
HuseyinCivan <huseyincivan@gmail.com>
2021-02-16 21:41:21 -0500

    Spring-11-Rest-Security-JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/AuthenticationRequest.java
@@ -0,0 +1,18 @@
+package com.cybertek.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
+
+
+}
