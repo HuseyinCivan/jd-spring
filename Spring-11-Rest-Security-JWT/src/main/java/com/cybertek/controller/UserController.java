637d7f7f2e139dcc561976da5e4d21fa23c85a45
HuseyinCivan <huseyincivan@gmail.com>
2021-02-18 23:01:36 -0500

    Spring-11-Rest-Security-JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
@@ -5,6 +5,8 @@ import com.cybertek.entity.User;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/read")
-    public ResponseEntity<ResponseWrapper> readAll() {
+    @PreAuthorize("hasAuthority('USER')")
+//    @PreAuthorize("hasAnyAuthority('USER','ADMIN')")
+    public ResponseEntity<ResponseWrapper> readAll(){
+
         List<User> users = userService.getAll();
-        return ResponseEntity.ok(new ResponseWrapper("Done", users));
+
+        return ResponseEntity.ok(new ResponseWrapper("Done",users));
 
     }
-
-
 }
---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/exception/ServiceException.java
@@ -0,0 +1,10 @@
+package com.cybertek.exception;
+
+public class ServiceException extends Exception{
+
+    public ServiceException(String message){
+        super(message);
+    }
+
+
+}
