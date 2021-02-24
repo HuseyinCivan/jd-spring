9743cc2fd5f447cfc2ab29a42b3c272fe8041fec
HuseyinCivan <huseyincivan@gmail.com>
2021-02-23 19:56:51 -0500

    Spring-12-AOP

--- Spring-12-AOP/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-12-AOP/src/main/java/com/cybertek/controller/ProductController.java
@@ -21,7 +21,7 @@ public class ProductController {
 
     private ProductService productService;
 
-    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
 
     public ProductController(ProductService productService) {
         this.productService = productService;
@@ -31,11 +31,11 @@ public class ProductController {
     @GetMapping
     public List<Product> getProducts() {
 
-        logger.info("Before -> Controller:{} - Method:{} - Input Parameter :{}", "ProductController", "getProducts()");
+
 
         List<Product> list = productService.getProducts();
 
-        logger.info("After -> Controller:{} - Method:{} - Output Parameters:{}", "ProductController", "getProducts", list.toString());
+
 
         return list;
     }
